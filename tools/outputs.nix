05b4a0f72b39bb1bcfab155dc3d48cc5a1dd31bf
Chris Martin <ch.martin@gmail.com>
2019-09-23 17:01:30 -0600

    add .ghci, enable warnings

--- tools/outputs.nix
+++ tools/outputs.nix
@@ -1,36 +1,34 @@
 { runCommand, linkFarm, haskell, lib }:
 
 let
-  inherit (lib) concatMapStringsSep escapeShellArg;
-
-  run = name: hsFile: run' name hsFile {};
-
-  run' = name: hsFile: { sed ? [] }:
-    {
-      inherit name;
-      path = runCommand "phrasebook-output-${name}.txt"
-        {
-          buildInputs = [ haskell ];
-          inherit hsFile;
-        }
-        ''
-          runhaskell "$hsFile" ${concatMapStringsSep " " (x: "| sed -e ${escapeShellArg x}") sed} > $out
+    run = name: hsFile: { sed ? [] }: {
+        inherit name;
+        path = runCommand "phrasebook-output-${name}.txt" { buildInputs = [ haskell ]; } ''
+            runhaskell ${lib.escapeShellArgs (ghcOptions ++ ["${hsFile}"])} ${lib.concatMapStringsSep " " (x: "| sed -e ${lib.escapeShellArg x}") sed} > $out
         '';
     };
 
-in
+    ghcOptions = [
+        "-Werror"
+        "-Wall"
+        "-fno-warn-missing-signatures"
+        "-fno-warn-name-shadowing"
+        "-fno-warn-type-defaults"
+        "-fno-warn-unused-do-bind"
+    ];
 
-  linkFarm "haskell-phrasebook-outputs" [
-    (run' "branching.txt" ../branching.hs { sed = ["s!^It's .* noon$!It's ... noon!"]; })
-    (run "common-types.txt" ../common-types.hs)
-    (run "crypto-hashing.txt" ../crypto-hashing.hs)
-    (run "dynamic.txt" ../dynamic.hs)
-    (run "for-loops.txt" ../for-loops.hs)
-    (run "hashing.txt" ../hashing.hs)
-    (run "hello-world.txt" ../hello-world.hs)
-    (run "mutable-references.txt" ../mutable-references.hs)
-    (run' "threads.txt" ../threads.hs { sed = ["s!^fork.*$!...!"]; })
-    (run "timeouts.txt" ../timeouts.hs)
-    (run' "transactions.txt" ../transactions.hs { sed = ["s!\\[.*\\]!...!"]; })
-    (run "variables.txt" ../variables.hs)
-  ]
+in
+    linkFarm "haskell-phrasebook-outputs" [
+        (run "branching.txt" ../branching.hs { sed = ["s!^It's .* noon$!It's ... noon!"]; })
+        (run "common-types.txt" ../common-types.hs {})
+        (run "crypto-hashing.txt" ../crypto-hashing.hs {})
+        (run "dynamic.txt" ../dynamic.hs {})
+        (run "for-loops.txt" ../for-loops.hs {})
+        (run "hashing.txt" ../hashing.hs {})
+        (run "hello-world.txt" ../hello-world.hs {})
+        (run "mutable-references.txt" ../mutable-references.hs {})
+        (run "threads.txt" ../threads.hs { sed = ["s!^fork.*$!...!"]; })
+        (run "timeouts.txt" ../timeouts.hs {})
+        (run "transactions.txt" ../transactions.hs { sed = ["s!\\[.*\\]!...!"]; })
+        (run "variables.txt" ../variables.hs {})
+    ]

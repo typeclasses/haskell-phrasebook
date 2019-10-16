4a2c1bd895a26311e83b9e5b68be5947e7dc14f3
Chris Martin <ch.martin@gmail.com>
2019-10-16 15:38:23 -0600

    time: go back to letting parse specialize to IO

--- time.hs
+++ time.hs
@@ -20,8 +20,8 @@ main =
     now_posix :: POSIXTime <- getPOSIXTime
     putStrLn ("Now (POSIX): " ++ show now_posix)
 
-    let Just (t1_utc :: UTCTime) =
-            parse "%Y-%m-%d %H:%M:%S" "2038-01-19 03:14:07"
+    t1_utc :: UTCTime <- parse "%Y-%m-%d %H:%M:%S"
+                               "2038-01-19 03:14:07"
     putStrLn (show t1_utc)
 
     putStrLn (render "%Y-%m-%d" t1_utc)

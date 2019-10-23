40689a4071b3d709d5dd6cda8806e3449ec1d5a9
Chris Martin <ch.martin@gmail.com>
2019-10-23 11:01:55 -0600

    records-with-optics: formatting opinions

--- records-with-optics.hs
+++ records-with-optics.hs
@@ -1,29 +1,38 @@
 {-# LANGUAGE TemplateHaskell #-}
 
-import           Optics
+import Optics
 
-data Address = Address
-  { _city    :: String
-  , _country :: String
-  } deriving (Show)
+data Address =
+  Address
+    { _city    :: String
+    , _country :: String
+    }
+  deriving (Show)
 
 makeLenses ''Address
 
-data Person = Person
-  { _name    :: String
-  , _age     :: Int
-  , _address :: Address
-  } deriving (Show)
+data Person =
+  Person
+    { _name    :: String
+    , _age     :: Int
+    , _address :: Address
+    }
+  deriving (Show)
 
 makeLenses ''Person
 
-main = do
-  let alice =
-        Person
-          {_name = "Alice", _age = 30, _address = Address "Faketown" "Fakeland"}
-  print alice
-  print (set age 40 alice)
-  print (over age (+ 1) alice)
-  print (view age alice)
-  print (view (address % city) alice)
-  print (set (address % city) "Fakeville" alice)
+main =
+  do
+    let alice =
+          Person
+            { _name = "Alice"
+            , _age = 30
+            , _address = Address "Faketown" "Fakeland"
+            }
+
+    print alice
+    print (set age 40 alice)
+    print (over age (+ 1) alice)
+    print (view age alice)
+    print (view (address % city) alice)
+    print (set (address % city) "Fakeville" alice)

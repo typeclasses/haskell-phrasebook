4b9e5fa5f89456aba7fa6c8a93db4def3215cd07
Chris Martin <ch.martin@gmail.com>
2020-05-19 18:29:25 -0600

    refactor outputs.nix to use builtins.derivation, add docs

--- tools/outputs.nix
+++ tools/outputs.nix
@@ -1,46 +1,87 @@
-{ runCommand, linkFarm, haskell, lib }:
+{ linkFarm, haskell, lib, system, writeShellScript, coreutils, gnused }:
 
 let
-    run = name: hsFile: { args ? [], pipes ? [] }: {
+
+# Each item in this list is an argument for the `phrasebookOutput` function below.
+examples = [
+    { name = "bounded-queues"; file = ../bounded-queues.hs; sed = "s!^(finish:.*|start: (6|7|8|9|10))$!...!"; }
+    { name = "branching"; file = ../branching.hs; sed = "s!^It's .* noon$!It's ... noon!"; }
+    { name = "common-types"; file = ../common-types.hs; }
+    { name = "crypto-hashing"; file = ../crypto-hashing.hs; }
+    { name = "dynamic"; file = ../dynamic.hs; }
+    { name = "enum-ranges"; file = ../enum-ranges.hs; }
+    { name = "file-handles"; file = ../file-handles.hs; }
+    { name = "folding-lists"; file = ../folding-lists.hs; }
+    { name = "for-loops"; file = ../for-loops.hs; }
+    { name = "functions"; file = ../functions.hs; }
+    { name = "hashing"; file = ../hashing.hs; }
+    { name = "hello-world"; file = ../hello-world.hs; }
+    { name = "invert"; file = ../invert.hs; }
+    { name = "mutable-references"; file = ../mutable-references.hs; }
+    { name = "moments-in-time"; file = ../moments-in-time.hs; sed = "s!(Now .*: ).*$!\\1...!"; }
+    { name = "monitoring"; file = ../monitoring.hs; args = ["full-demonstration"]; }
+    { name = "records-with-optics"; file = ../records-with-optics.hs; }
+    { name = "threads"; file = ../threads.hs; sed = "s!^fork.*$!...!"; }
+    { name = "timeouts"; file = ../timeouts.hs; }
+    { name = "transactions"; file = ../transactions.hs; sed = "s!\\[.*\\]!...!"; }
+    { name = "variables"; file = ../variables.hs; }
+];
+
+outputs = linkFarm "haskell-phrasebook-outputs" (builtins.map phrasebookOutput examples);
+
+# The output file for a Phrasebook example program.
+phrasebookOutput =
+    { name # The name of the output file (with no file extension)
+    , file # The Haskell source file to run to produce the output
+    , args ? [] # List of arguments for the Haskell program
+    , sed ? null # A sed script to run on the program output (we use this to filter non-deterministic output from programs that employ concurrency or randomness, so that the derivations contain only consistently reproducible results)
+    }:
+    {
         name = "${name}.txt";
-        path = runCommand "phrasebook-output-${name}.txt" { buildInputs = [ haskell ]; } ''
-            ln -s ${hsFile} ${name}.hs
-            runhaskell ${lib.escapeShellArgs (["--"] ++ ghcOptions ++ ["--"] ++ ["${name}.hs"] ++ args)} ${lib.concatMapStringsSep " " (x: "| ${x}") pipes} > $out
+        path = runghc {
+            inherit name;
+            hsFile = file;
+            programArgs = args;
+            pipes = if sed != null then ["${gnused}/bin/sed -re ${lib.escapeShellArg sed}"] else [];
+            ghcArgs = [
+                "-Werror"
+                "-Wall"
+                "-fno-warn-missing-signatures"
+                "-fno-warn-name-shadowing"
+                "-fno-warn-type-defaults"
+                "-fno-warn-unused-do-bind"
+            ];
+        };
+    };
+
+# A derivation created by running a Haskell file and piping the output to a file
+runghc =
+    { name # A string that appears as part of the path under /nix/store to help identify what is being built
+    , hsFile # The path of the Haskell file to run
+    , programArgs ? [] # List of arguments for the Haskell program
+    , runghcArgs ? [] # List of arguments for runghc
+    , ghcArgs ? [] # List of arguments for GHC
+    , pipes ? [] # List of additional shell commands to pipe the output through
+    }:
+    derivation {
+        name = "runghc-${name}";
+        path = ["${haskell}/bin" "${coreutils}/bin"];
+        buildScript = writeShellScript "builder-for-runghc-${name}" ''
+            ${coreutils}/bin/ln -s ${hsFile} ${name}.hs
+            ${haskell}/bin/runghc ${lib.escapeShellArgs (runghcArgs ++ ["--"] ++ ghcArgs ++ ["--"] ++ ["${name}.hs"] ++ programArgs)} ${lib.concatMapStringsSep " " (x: "| ${x}") pipes} > $out
         '';
     };
 
-    ghcOptions = [
-        "-Werror"
-        "-Wall"
-        "-fno-warn-missing-signatures"
-        "-fno-warn-name-shadowing"
-        "-fno-warn-type-defaults"
-        "-fno-warn-unused-do-bind"
-    ];
+# A small wrapper around the built-in `derivation` function
+derivation =
+    { name # A string that appears as part of the path under /nix/store to help identify what is being built
+    , buildScript # The shell script used to build the derivation
+    , path ? [] # A list of directories to include on the binary path for the build script
+    }:
+    builtins.derivation {
+        inherit name system;
+        PATH = lib.concatStringsSep ":" path;
+        builder = writeShellScript "builder-for-${name}" buildScript;
+    };
 
-    sed = x: "sed -re ${lib.escapeShellArg x}";
-
-in
-    linkFarm "haskell-phrasebook-outputs" [
-        (run "bounded-queues" ../bounded-queues.hs { pipes = [(sed "s!^(finish:.*|start: (6|7|8|9|10))$!...!")]; })
-        (run "branching" ../branching.hs { pipes = [(sed "s!^It's .* noon$!It's ... noon!")]; })
-        (run "common-types" ../common-types.hs {})
-        (run "crypto-hashing" ../crypto-hashing.hs {})
-        (run "dynamic" ../dynamic.hs {})
-        (run "enum-ranges" ../enum-ranges.hs {})
-        (run "file-handles" ../file-handles.hs {})
-        (run "folding-lists" ../folding-lists.hs {})
-        (run "for-loops" ../for-loops.hs {})
-        (run "functions" ../functions.hs {})
-        (run "hashing" ../hashing.hs {})
-        (run "hello-world" ../hello-world.hs {})
-        (run "invert" ../invert.hs {})
-        (run "mutable-references" ../mutable-references.hs {})
-        (run "moments-in-time" ../moments-in-time.hs { pipes = [(sed "s!(Now .*: ).*$!\\1...!")]; })
-        (run "monitoring" ../monitoring.hs { args = ["full-demonstration"]; })
-        (run "records-with-optics" ../records-with-optics.hs {})
-        (run "threads" ../threads.hs { pipes = [(sed "s!^fork.*$!...!")]; })
-        (run "timeouts" ../timeouts.hs {})
-        (run "transactions" ../transactions.hs { pipes = [(sed "s!\\[.*\\]!...!")]; })
-        (run "variables" ../variables.hs {})
-    ]
+in outputs

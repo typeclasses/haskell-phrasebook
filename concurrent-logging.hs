6f9f40442b6c4aeb09b51c22c49e0d6e77089461
Chris Martin <ch.martin@gmail.com>
2022-02-22 16:48:00 -0700

    concurrent-logging: make sure all log messages print
    Use concurrently_ and forConcurrently_ from the async package instead
    of forkIO.
    
    Use a TVar to tell the printing thread when to stop instead of just
    stopping when the queue is empty, because an empty queue doesn't
    necessarily mean nothing else will get logged.
    
    Use a continuation passing style to obtain the concurrent log action,
    because this is encouraged by the async package to ensure that all
    threads get canceled in the event of an exception.

--- concurrent-logging.hs
+++ concurrent-logging.hs
@@ -1,46 +1,52 @@
+import Control.Applicative (liftA2)
+import Control.Monad (unless, replicateM_)
 import Control.Concurrent (forkIO, threadDelay)
+import Control.Concurrent.Async (concurrently_, forConcurrently_)
 import Control.Monad.STM (atomically)
 import Data.Foldable (for_)
 import System.Environment (lookupEnv)
 import System.Random (randomRIO)
 import Text.Read (readMaybe)
 
-import qualified Control.Concurrent.STM.TBQueue as TBQ
+import qualified Control.Concurrent.STM.TQueue as TQ
+import qualified Control.Concurrent.STM.TVar as TV
 
-seconds n = 1000000 * n
+randomDelay =
+  do
+    i <- randomRIO (1, 1000)
+    threadDelay i
 
-threadsafeLog maxQueue = do
-  queue <- atomically (TBQ.newTBQueue maxQueue)
+-- Here we have a concurrent program that is parameterized on how to write log messages.
+choir log =
+    -- Run three concurrent threads, each singing a different tune.
+    forConcurrently_ ["Do Re Mi", "Fa Sol", "La Ti Do"] $ \tune ->
+        replicateM_ 3 $ -- Each member of the choir sings its tune 3 times.
+          do
+            randomDelay -- ... with some random delays between each repetition.
+            log tune
 
-  let
-    logToQueue msg = atomically (TBQ.writeTBQueue queue msg)
+-- Our demonstration program runs the "choir" two different ways:
+main =
+  do
+    choir putStrLn -- The first way has a serious flaw, which we shall see in the output.
+    putStrLn "---"
+    withConcurrentLog choir -- The second way uses a queue to orchestrate the printing.
 
-    printFromQueue = do
-      threadDelay (seconds 1 `div` 2)
-      emptyQueue <- atomically (TBQ.isEmptyTBQueue queue)
-      if emptyQueue then
-        return ()
-      else do
-        msg <- atomically (TBQ.readTBQueue queue)
-        putStrLn msg
-        printFromQueue
+withConcurrentLog go = do
+    queue <- TQ.newTQueueIO
+    stopVar <- TV.newTVarIO False
 
-  return (logToQueue, printFromQueue)
+    let
+      logToQueue msg = atomically (TQ.writeTQueue queue msg)
 
-sing phrase log =
-  for_ [1..5] $ \n -> do
-    i <- randomRIO (1, 10000)
-    threadDelay (seconds 1 `div` i + n)
-    log phrase
+      printFromQueue = do
+        randomDelay
+        stop <- atomically (liftA2 (&&) (TQ.isEmptyTQueue queue) (TV.readTVar stopVar))
+        unless stop $ do
+          msg <- atomically (TQ.readTQueue queue)
+          putStrLn msg
+          printFromQueue
 
-main = do
-  (log, print) <- do
-    maxQueue <- (readMaybe =<<) <$> lookupEnv "MAX_QUEUE"
-    case maxQueue of
-      Nothing -> return (putStrLn, threadDelay (seconds 3))
-      Just n -> threadsafeLog n
+    let stop = atomically (TV.writeTVar stopVar True)
 
-  for_ [sing "Do Re Mi", sing "Fa Sol", sing "La Ti Do"] $
-    \singPhrase -> forkIO (singPhrase log)
-
-  print
+    concurrently_ printFromQueue (go logToQueue *> stop)

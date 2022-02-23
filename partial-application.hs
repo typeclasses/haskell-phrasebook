2b021cf51a0dbdae5f14db74bdd03444ba7dbe86
Chris Martin <ch.martin@gmail.com>
2022-02-23 14:14:16 -0700

    partial-application: get into a compiling state

--- partial-application.hs
+++ partial-application.hs
@@ -1,5 +1,7 @@
-    map next [1..5]
-    map (\x -> x + 1) [1..5]
+main = undefined
+
+    -- map next [1..5]
+    -- map (\x -> x + 1) [1..5]
 
 -- CURRYING:
 -- f :: X ->  Y ->  Z -> A
@@ -11,22 +13,22 @@
 
 -- currying explained with lambdas:
 
-    \x y z -> x + y + z
+    -- \x y z -> x + y + z
 
 -- the same as
 
-    \x -> (\y z -> x + y + z)
+    -- \x -> (\y z -> x + y + z)
 
 -- the same as
 
-    \x -> (\y -> (\z -> x + y + z))
+    -- \x -> (\y -> (\z -> x + y + z))
 
 -- because all functions can be seen as functions with single argument, partial application is possible
 
-    add x y = x+y
-    add x y = (add x) y
-    add3    = add 3     -- = \y -> 3 + y
-    add3 4              -- = (add 3) 4 = add 3 4 = 7
+    -- add x y = x+y
+    -- add x y = (add x) y
+    -- add3    = add 3     -- = \y -> 3 + y
+    -- add3 4              -- = (add 3) 4 = add 3 4 = 7
 
 -- this is also the reason why it is possible to write things like this:
-    map (+1) [1..5]
+    -- map (+1) [1..5]
